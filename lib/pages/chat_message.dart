3c48e5b59cda82facfc36604280250ddb9c36715
Alvaro Tito <mainalvarotito@gmail.com>
2020-05-25 16:26:25 -0300

    ajustes

--- lib/pages/chat_message.dart
+++ lib/pages/chat_message.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatefulWidget {
-
   ChatMessage(this.mine, this.documents);
 
   final mine;
@@ -15,14 +14,15 @@ class _ChatMessageState extends State<ChatMessage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      //color: widget.mine? Colors.blue[100]: Colors.grey[300],
+      color: widget.mine ? Colors.blue[100] : Colors.grey[300],
       padding: EdgeInsets.symmetric(vertical: 10.0),
       margin: EdgeInsets.symmetric(vertical: 3.0),
       width: MediaQuery.of(context).size.width * 0.10,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
-          crossAxisAlignment: widget.mine ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          crossAxisAlignment:
+              widget.mine ? CrossAxisAlignment.end : CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               widget.documents['from'],
@@ -30,17 +30,27 @@ class _ChatMessageState extends State<ChatMessage> {
                 fontSize: 13,
               ),
             ),
-            SizedBox(height: 5,),
-            widget.documents['imgFile'] != null ? 
-            Image.network(
-              widget.documents['imgFile'], 
-              width: MediaQuery.of(context).size.width * 0.80,
-              height: MediaQuery.of(context).size.height * 0.80,
-            ):
+            SizedBox(
+              height: 5,
+            ),
+            widget.documents['imgFile'] != null
+                ? Image.network(
+                    widget.documents['imgFile'],
+                    width: MediaQuery.of(context).size.width * 0.80,
+                    height: MediaQuery.of(context).size.height * 0.80,
+                  )
+                : Text(
+                    widget.documents['text'],
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
             Text(
-              widget.documents['text'],
+              widget.documents['time'].hour.toString() +
+                  ':' +
+                  widget.documents['time'].minute.toString(),
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 8,
               ),
             ),
           ],
